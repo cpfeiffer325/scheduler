0528ed567a5fb04d38d41fb5f5884a444a2314f3
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-16 15:33:38 -0600

    add classNames object for conditional rendering

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import classNames from "classnames"
+
+import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day", {
+    "day-list__item": props,
+    "day-list__item--selected": props.selected === true,
+    "day-list__item--full": props.spots === 0
+  })
+  
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass}
+        onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--ligth">{props.spots}</h3>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   )
 }
