e0d7209b1633f683735fc124cb6f4ce3dd7fb018
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-18 17:19:41 -0600

    fix story to output selected interviewer id

--- stories/index.js
+++ stories/index.js
@@ -104,7 +104,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={action("setInterviewer")(interviewer.id)}
     />
   ))
 
