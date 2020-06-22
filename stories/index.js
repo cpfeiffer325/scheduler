03b5bbfafa256cd3ba243418ce1519f9ecf21379
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-22 14:11:57 -0600

    add show component for Appointment and story

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerList from "components/InterviewerList"
 import InterviewerListItem from "components/InterviewerListItem"
 import Appointment from "components/Appointment"
 import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 // Stories for the Button Component
 storiesOf("Button", module)
@@ -143,6 +145,13 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show 
+      student="Mildred Nazir"
+      interviewer={interviewers[3]}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
