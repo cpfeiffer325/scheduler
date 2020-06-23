a7a0c738d94f01ab1abebc3bd40f549c75758eaa
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-23 09:22:53 -0600

    add stories for Appointment Booked and Empty

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Fragment } from "react"
 
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
@@ -149,6 +149,22 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => (
@@ -196,3 +212,4 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
+
