f0b71f2132a40ef8f9437ac9a62ce1e537fec6a2
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-23 09:22:19 -0600

    add rendering of Header, Show and Empty in Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import Header from './Header'
+import Show from './Show'
+import Empty from './Empty'
+
 import "./styles.scss"
 
 export default function Appointment(props) {
   return (
-    <article className="appointment">Appointment</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview 
+        ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> 
+        : <Empty />
+      }
+    </article>
   )
 }
