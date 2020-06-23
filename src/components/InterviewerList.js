c3dd4548388c3fcc98f4eeb67ea1ae949778fc00
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-22 20:57:24 -0600

    change InterviewList to a controlled component for setInterview

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -12,8 +12,8 @@ export default function InterviewerList(props) {
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     )
   })
