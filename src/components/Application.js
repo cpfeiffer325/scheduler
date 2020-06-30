e185f6a3db316f3fe340ca5227ee2c306b5f088a
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-06-30 08:06:44 -0600

    update state object with api request for interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,68 +8,12 @@ import { getAppointmentsForDay } from '../helpers/selectors'
 
 import "components/Application.scss"
 
-// const appointments = [
-//   {
-//     key: 1,
-//     id: 1,
-//     time: "10am",
-//   },
-//   {
-//     key: 2,
-//     id: 2,
-//     time: "11am",
-//     interview: {
-//       student: "Dirt Squirrel",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png",
-//       }
-//     }
-//   },
-//   {
-//     key: 3,
-//     id: 3,
-//     time: "12pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     key: 4,
-//     id: 4,
-//     time: "1pm",
-//   },
-//   {
-//     key: 5,
-//     id: 5,
-//     time: "2pm",
-//     interview: {
-//       student: "Bob Loblaw",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     key: "last",
-//     id: 6,
-//     time: "5pm"
-//   }
-// ];
-
 export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
   
   const setDay = day => setState({ ...state, day })
@@ -77,9 +21,10 @@ export default function Application() {
   useEffect(() => {
     Promise.all([
       axios.get(`/api/days`),
-      axios.get(`/api/appointments`)
-    ]).then(([{data: days}, {data: appointments}]) => {
-      setState(prev => ({day: prev.day, days, appointments}))
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
+    ]).then(([{data: days}, {data: appointments}, {data: interviewers}]) => {
+      setState(prev => ({day: prev.day, days, appointments, interviewers}))
     })
   },[])
 
