b99f48601996e0991935ff53511a9a0be9e64e44
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-11-30 21:32:16 -0700

    create save funtion for appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Empty from './Empty'
 import Form from './Form'
 import Header from './Header'
 import Show from './Show'
+import Status from './Status'
 
 import useVisualMode from "hooks/useVisualMode"
 import "./styles.scss"
@@ -11,12 +12,16 @@ import "./styles.scss"
 
 
 export default function Appointment({ 
+  bookInterview,
+  id,
   interview,
-  interviewers, 
+  interviewers,
   time 
 }) {
   const CREATE = "CREATE"
   const EMPTY = "EMPTY"
+  const ERROR_SAVE = "ERROR_SAVE"
+  const SAVING = "SAVING"
   const SHOW = "SHOW"
   
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY)
@@ -24,26 +29,46 @@ export default function Appointment({
   const onAdd = () => {
     transition(CREATE)
   }
-
+  
   const onCancel = () => {
     back();
+  }
+  
+  function onSave(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    }
+    
+    transition(SAVING)
+    bookInterview(id, interview)
+      .then(() => transition(SHOW))
+      .catch(() => transition(ERROR_SAVE, true))
   }
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === EMPTY && (
+        <Empty onAdd={onAdd}
+      />
+      )}
       {mode === SHOW && (
         <Show
-          interviewers={interviewers}
+          student={interview.student}
           interviewer={interview.interviewer}
         />
       )}
       {mode === CREATE && (
         <Form
           interviewers={interviewers}
-          // onSave={onSave} 
+          onSave={onSave} 
           onCancel={onCancel}
+        />
+      )}
+      {mode === SAVING && (
+        <Status 
+          message={"Saving Appointment"}
         />
       )}
     </article>
